d3429d8d7e799521e3cb3bcdc21a4ec2ea5b719d
reza rastegar <moidreza82@gmail.com>
2023-08-08 23:10:55 +0330

    12

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 export default function News() {
   return (
     <>
-      <h1>News</h1>
+      <Fragment>
+        <h1>News</h1>
+        <ul>
+          <li>
+            <Link href="/news/blog-1">Blog 1</Link>
+          </li>
+          <li>
+            <Link href="/news/blog-2">Blog 2</Link>
+          </li>
+          <li>
+            <Link href="/news/blog-3">Blog 3</Link>
+          </li>
+        </ul>
+      </Fragment>
       <p>lorem</p>
     </>
   );
